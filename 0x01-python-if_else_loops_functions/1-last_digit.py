9b6aa2432836e8817fa4fe39032bbbbec55e0d32
kwameobj <kowusubamfo@gmail.com>
2023-06-07 12:53:06 +0000

    Updated Task 1 - to make the generated random numbers absolute values before finding the last number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_num = number % 10
+last_num = abs(number) % 10
 if last_num > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, last_num))
 elif last_num == 0:
