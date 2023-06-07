7565b2896c71819cc6da78950d812b8c3b96ad3e
kwameobj <kowusubamfo@gmail.com>
2023-06-07 12:45:51 +0000

    Updated Task 1 - to print additional text when the random number is greater than 5

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,8 +3,8 @@ import random
 number = random.randint(-10000, 10000)
 last_num = number % 10
 if last_num > 5:
-    print("Last digit of {} is {}".format(number, last_num))
+    print("Last digit of {} is {} and is greater than 5".format(number, last_num))
 elif last_num == 0:
-    print("Last digit of {} is 0 and is 0".format(number))
+    print("Last digit of {} is {} and is 0".format(number, last_num))
 elif last_num < 6 and last_num != 0:
     print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_num))
