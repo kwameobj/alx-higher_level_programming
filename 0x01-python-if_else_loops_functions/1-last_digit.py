9f386cd4b2029f4c682a316a07c15073755cbeb2
kwameobj <kowusubamfo@gmail.com>
2023-06-07 13:24:14 +0000

    Updated Task 1 - to satisfy Pycodestyle

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
+
+#1-last_digit.py - a python code that randomly generates a number
+#                  and prints its last number with some information
+
 import random
 number = random.randint(-10000, 10000)
+str_1 = "Last digit of {} is {} and is "
+
+
 if number >= 0:
     last_num = number % 10
 elif number < 0:
     last_num = (abs(number) % 10) * -1
+
 if last_num > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, last_num))
+    print((str_1 + "greater than 5").format(number, last_num))
 elif last_num == 0:
-    print("Last digit of {} is {} and is 0".format(number, last_num))
+    print((str_1 + "0").format(number, last_num))
 elif last_num < 6 and last_num != 0:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_num))
+    print((str_1 + "less than 6 and not 0").format(number, last_num))
