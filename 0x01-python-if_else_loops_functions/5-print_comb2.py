ff95485df676484e2840bfc749d321a529c871e3
kwameobj <kowusubamfo@gmail.com>
2023-06-08 05:34:52 +0000

    Updated 5-print_comb2.py - to remove double pasting

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,12 +3,4 @@ for i in range(100):
     if i == 99:
         print("{}".format(i))
         continue
-
-    print("{:02}".format(i), end=", ")#!/usr/bin/python3
-
-for i in range(100):
-    if i == 99:
-        print("{}".format(i))
-        continue
-
     print("{:02}".format(i), end=", ")
