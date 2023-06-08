3698fdd71ddd5294ed33f2faee73434b730a58ba
kwameobj <kowusubamfo@gmail.com>
2023-06-08 12:53:06 +0000

    Updated 100-point_tebahapla.py to look like mine

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(122, 96, 64):
     if i % 2 == 0:
-        print('{}'.format(chr(i)), end='')
+        print("{}".format(chr(i)), end="")
         continue
-    print('{}'.format(chr(i - 32)), end='')
+    print("{}".format(chr(i - 32)), end="")
