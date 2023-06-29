8c1a74f3e2dd63a5f6596aa2af49e0b9c57068be
kwameobj <kowusubamfo@gmail.com>
2023-06-29 06:14:15 +0000

    Updated Task 0 file to satisfy docstring documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 class Square:
-    """Creates an empty class Square"""
+    """Creates an empty class Square.
+
+    This is only necessary to make an empty class to pass the checks for task 0.
+    """
     pass
