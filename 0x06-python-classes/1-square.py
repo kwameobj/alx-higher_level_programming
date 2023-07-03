2fc99482192a6a752209b8a43abb4eb1d434d98e
kwameobj <kowusubamfo@gmail.com>
2023-07-03 18:36:23 +0000

    Updated Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-class Square:
-    """ Creates a class called Square
+"""A file that contians python code to create a class
+   and name it Square
+   """
 
+
+class Square:
+    """Creates a class Square.
+
+    Attributes:
+        size (int): the length of one side of the square
     """
 
     def __init__(self, size):
+        """Initializes the attritubes of the class
+
+           Args:
+                size(int): The size of the class square
+        """
         self.__size = size
-    """ Initializes the attritubes of the class
-
-    Args:
-        size(int): The size of the class square
-
-    """
