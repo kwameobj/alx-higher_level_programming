f0463f8ce92d841bf9946df7bc58f9fedaf47d9d
Kwame Owusu-Bamfo <90472173+kwameobj@users.noreply.github.com>
2023-07-03 17:40:33 +0000

    Task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,15 +4,15 @@ class Square:
 
     """
 
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """Initializes the attritube "size" of the class.
 
         Args:
             size(int): The size of the class square
 
         """
-        
-        if(type(size) is not int):
+
+        if (type(size) is not int):
             raise TypeError("size must be an integer")
         if(size < 0):
             raise ValueError("size must be >=0")
