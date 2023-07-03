0379172c4a2d3a39e1d902bf279d18aab187db59
kwameobj <kowusubamfo@gmail.com>
2023-07-03 16:43:26 +0000

    Task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,6 +2,7 @@
 class Square:
     """Creates an empty class Square.
 
-    This is only necessary to make an empty class to pass the checks for task 0.
-    """
+    This is only necessary to make an empty class
+    to pass the checks for task 0."""
+
     pass
