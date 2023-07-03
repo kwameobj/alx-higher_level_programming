109841d77c91077e8f2c8271f90e394a67aaf24b
kwameobj <kowusubamfo@gmail.com>
2023-07-03 18:46:23 +0000

    Task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -19,9 +19,9 @@ class Square:
             size (int): size of the class square which
 
         """
-        
-        if(type(size) is not int):
+
+        if (type(size) is not int):
             raise TypeError("size must be an integer")
-        if(size < 0):
+        if (size < 0):
             raise ValueError("size must be >=0")
         self.__size = size
