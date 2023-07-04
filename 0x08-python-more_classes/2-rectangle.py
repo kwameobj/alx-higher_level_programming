7ebe0313b478085d0e18a0e08a656c241db812a2
kwameobj <kowusubamfo@gmail.com>
2023-07-04 06:10:09 +0000

    Updated teask 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -68,4 +68,4 @@ class Rectangle:
         """
         if self.width == 0 or self.height == 0:
             return 0
-        return (2 * self.width) + (2* self.height)
+        return (2 * self.width) + (2 * self.height)
