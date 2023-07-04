c358764fb00a9d4c550d41026defabc1be6f3e4a
kwameobj <kowusubamfo@gmail.com>
2023-07-04 08:28:30 +0000

    Task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Creates a class square"""
+
+
+class Square:
+    """Creates a class called Square"""
+
+    def __init__(self, size=0):
+        """Initializes the attritube(s) of the class Square
+
+        Args:
+            size(int): The size of the class square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Gets a private attribute size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the value of private attribute size and
+           raises an error is it is not a positive integer"""
+        if (type(value) is not int):
+            raise TypeError("size must be an integer")
+        if (value < 0):
+            raise ValueError("size must be >=0")
+        self.__size = value
+
+    def area(self):
+        """Returns the area of the square"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """prints in stdout the square with the character #"""
+        if (self.size == 0):
+            return ""
+        for i in range(self.size):
+            print("#" * self.size, end="")
+            print()
+        
