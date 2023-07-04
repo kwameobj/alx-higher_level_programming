0ec8948639b3f4351194976eca5c0a40a5fbd33d
kwameobj <kowusubamfo@gmail.com>
2023-07-04 06:12:47 +0000

    Satisfied pycode

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -68,7 +68,7 @@ class Rectangle:
         """
         if self.width == 0 or self.height == 0:
             return 0
-        return (2 * self.width) + (2* self.height)
+        return (2 * self.width) + (2 * self.height)
 
     def __str__(self):
         """print the rectangle with the character #
