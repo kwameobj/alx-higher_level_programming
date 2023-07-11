795bf0d432ed079ece8fe01be09691229abcc15e
kwameobj <kowusubamfo@gmail.com>
2023-07-11 05:08:53 +0000

    Updated Task 0 zero file to suit pycode style

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """returns the list of available attributes and methods of an object"""
+
+
 def lookup(obj):
     """
     lookup returns the list of available attributes and methods
-    
 
     Args:
         Obj(object): An object to be parsed
