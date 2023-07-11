0c16faeedf35997bba294b03a593d73c99baa6c3
kwameobj <kowusubamfo@gmail.com>
2023-07-11 20:06:12 +0000

    Updated 9-rectangle.py (Task 9) file

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""a class Rectangle that inherits from BaseGeometry"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Creates class Rectangle that inherits BaseGeometry"""
+
+    def __init__(self, width, height):
+        """
+        Initialises the attributes width and height and
+        validates if they are positive integers
+
+        Args:
+            width (int): measure of one side of the rectangle
+            heing (int): measure of one side of the rectangle
+
+        Return:
+            The class
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """Returns the are of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """prints rectangle description"""
+        return "[Rectangle] = {}/{}".format(self.__width, self.__height)
