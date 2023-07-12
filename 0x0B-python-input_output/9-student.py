fa2e1109577a2e97e74bac266669c263eb31dec4
kwameobj <kowusubamfo@gmail.com>
2023-07-12 05:18:23 +0000

    Added Task 9 file

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""creates the class Student"""
+
+
+class Student:
+    """creates the class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """initalizes new Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        retrieves dictionary representation of a
+        Student instance
+        """
+        return self.__dict__
