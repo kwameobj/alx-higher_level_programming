7164b573ef1ba821baaa0c2cbf06436ce11634c7
kwameobj <kowusubamfo@gmail.com>
2023-07-12 01:34:03 +0000

    Updated Task 1 and 2 files

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""1-write_file.py - writes a string to a txt file"""
+"""writes a string to a text file"""
 
 
 def write_file(filename="", text=""):
     """
-    writes a string to a text file (UTF8) and returns
-    the number of characters written
+    writes a string to a text file (UTF8) and
+    returns the number of characters written
     """
-    count = 0
-    with open(filename, mode='r' encoding='utf-8') as f:
-        for line in f:
-            count += 1
-    return count
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-""" contains the read_lines function """
+""" appends a string at the end of a text file"""
 
 
-def read_lines(filename="", nb_lines=0):
-    """ reads a n lines of a text file and prints them to stdout """
-    count = 0
-    with open(filename, encoding='utf-8') as f:
-        for line in f:
-            count += 1
-        f.seek(0)
-        if 0 < nb_lines < count:
-            for i in range(nb_lines):
-                print(f.readline(), end="")
-                i += 1
-        else:
-            for line in f:
-                print(line, end="")
+def append_write(filename="", text=""):
+    """
+    appends a string at the end of a text file (UTF8)
+    and returns the number of characters added
+    """
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
