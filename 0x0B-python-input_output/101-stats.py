718588f56f77f75822c9b6bc934b7366e66d68d6
kwameobj <kowusubamfo@gmail.com>
2023-07-12 05:56:48 +0000

    Altered Task 14 file - 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""reads stdin line by line and computes metrics"""
+"""Script reads stdin line by line and computes metrics"""
+
+
 import sys
 
 
-def print_size_and_statcodes(size, stat_codes):
-    """print file size and status codes"""
+def print_size_and_codes(size, stat_codes):
     print("File size: {:d}".format(size))
     for k, v in sorted(stat_codes.items()):
         if v:
@@ -12,7 +13,6 @@ def print_size_and_statcodes(size, stat_codes):
 
 
 def parse_stdin_and_compute():
-    """computing function"""
     size = 0
     lines = 0
     stat_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
@@ -25,12 +25,12 @@ def parse_stdin_and_compute():
                 stat_codes[fields[-2]] += 1
             lines += 1
             if lines % 10 == 0:
-                print_size_and_statcodes(size, stat_codes)
+                print_size_and_codes(size, stat_codes)
     except KeyboardInterrupt:
-        print_size_and_statcodes(size, stat_codes)
+        print_size_and_codes(size, stat_codes)
         raise
 
-    print_size_and_statcodes(size, stat_codes)
+    print_size_and_codes(size, stat_codes)
 
 
 parse_stdin_and_compute()
