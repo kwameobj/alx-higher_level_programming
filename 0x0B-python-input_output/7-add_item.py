c0355f60b1f710ca7025366fba157205a2282c3c
kwameobj <kowusubamfo@gmail.com>
2023-10-28 08:16:24 +0000

    Uploading

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,23 +1,9 @@
 #!/usr/bin/python3
-"""
-script to save and load
-"""
-from sys import argv
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+""" contains the save_to_json_file function """
+import json
 
 
-filename = 'add_item.json'
-my_list = []
-try:
-    my_list = load_from_json_file(filename)
-except Exception:
-    save_to_json_file(my_list, filename)
-
-arg_len = len(argv)
-
-if arg_len > 1:
-    for i in range(1, arg_len):
-        my_list.append(argv[i])
-
-    save_to_json_file(my_list, filename)
+def save_to_json_file(my_obj, filename):
+    """ writes an obj to a text file, using json representation """
+    with open(filename, 'w', encoding='utf-8') as f:
+        return json.dump(my_obj, f)
