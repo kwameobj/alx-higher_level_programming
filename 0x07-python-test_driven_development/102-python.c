c682135f3a163b31a9be90b34aa360dc44de0965
kwameobj <kowusubamfo@gmail.com>
2023-11-30 10:26:44 +0000

    Updated Task 8 file(s)

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -4,13 +4,13 @@
 
 /**
  * print_python_string - Prints string information
- * 
+ *
  * @p: Python Object
  * Return: no return
  */
 void print_python_string(PyObject *p)
 {
-	
+
 	PyObject *str, *repr;
 
 	(void)repr;
