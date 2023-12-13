6ca2d8b15512347befa4701c292b11ff0202b627
kwameobj <kowusubamfo@gmail.com>
2023-12-13 17:22:45 +0000

    Updated Task 7 file(s)

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -61,7 +61,7 @@ class Rectangle:
         """returns string representation of Rectangle"""
         string = ""
         if self.__width != 0 and self.__height != 0:
-            string += "\n".join(str(print_symbol) * self.__width
+            string += "\n".join(str(self.print_symbol) * self.__width
                                 for j in range(self.__height))
         return string
 
