24dce0c53e18e8aaa7985e9a8be3fd62ff1b4bb4
kwameobj <kowusubamfo@gmail.com>
2023-12-14 08:21:15 +0000

    Added main files

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -6,7 +6,9 @@ Unittests for Base Class
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
-import unittest, sys, json
+import unittest
+import sys
+import json
 from unittest.mock import patch
 from io import StringIO
 import os
