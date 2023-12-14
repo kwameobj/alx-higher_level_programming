0448807aa96380e36aea913eda8b15b5a508961e
kwameobj <kowusubamfo@gmail.com>
2023-12-14 08:03:50 +0000

    Added task 2 file(s)

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""defines a Rectangle class"""
+import base.py
+
+
+class Rectangle(Base):
+    """Rectangle class"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        initialization method
+
+        args:
+            width: width of rectangle
+            height: width of rectangle
+            x: init variable
+            y: init variable
+        """
+        super().__init__()
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """private instance attribute width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter for private instance attribute width"""
+        return self.__width = value
+
+    @property
+    def height(self):
+        """private instance attribute height"""
+        return self.__height
+
+    @height.setter
+    def width(self, value):
+        """setter for private instance attribute height"""
+        return self.__height = value
+
+    @property
+    def x(self):
+        """private instance attribute x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """setter for private instance attribute x"""
+        return self.__x = value
+
+    @property
+    def y(self):
+        """private instance attribute y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """setter for private instance attribute y"""
+        return self.__y = value
